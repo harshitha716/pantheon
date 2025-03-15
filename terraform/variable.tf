e288be4b73935b4ebd8d85af3cec4fa4a99ba965
harshitha716 <harshithapendiyala0812@gmail.com>
2025-03-16 02:14:14 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,8 @@
 variable "AWS_REGION" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
 
-variable "AWS_ACCOUNT_ID" {
-  description = "AWS account ID"
-  type        = string
-}
+
 
